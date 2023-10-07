ac2f352cd47f5a8dd648bef41934b5df56d468ec
adhy-p <adhyp02@gmail.com>
2023-10-08 00:30:41 +0800

    second stage

--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client, addr = server_socket.accept()
-    client.recv(1)
-    client.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    client.recv(1024)
+    client.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
