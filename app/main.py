8d7647367680f14ff9acae3720b014cb9749c5d4
adhy-p <adhyp02@gmail.com>
2023-10-08 00:27:28 +0800

    second stage

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-# Uncomment this to pass the first stage
 import socket
 
 
@@ -6,7 +5,10 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()
+    client, addr = server_socket.accept()
+    while True:
+        client.recv(2048)
+        client.send(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
