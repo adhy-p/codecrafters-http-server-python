06e30f9a15cb45a5abf2d6b7fe262b2217225e00
adhy-p <adhyp02@gmail.com>
2023-10-08 13:59:24 +0800

    fifth stage

--- app/main.py
+++ app/main.py
@@ -6,8 +6,33 @@ CONTENT_TYPE = b"Content-Type: text/plain\r\n"
 END = b"\r\n"
 
 
+def parse_headers(headers) -> dict:
+    headers = headers.split(b"\r\n")
+    method, path, version = headers[0].split()  # status line
+    parsed_header = {
+        'method': method,
+        'path': path,
+        'version': version,
+        }
+    for header in headers[1:]:
+        key, *value = [h.strip() for h in header.split(b':')]
+        parsed_header[key.decode()] = b"".join(value)
+    return parsed_header
+
+
 def gen_content_len(n: int):
     return f"Content-Length: {n}\r\n".encode()
+
+
+def handle_echo(path, echo_idx):
+    message = path[echo_idx + len("/echo/"):]
+    resp_header = OK + CONTENT_TYPE + gen_content_len(len(message)) + END
+    return resp_header + message
+
+
+def handle_user_agent(agent):
+    resp_header = OK + CONTENT_TYPE + gen_content_len(len(agent)) + END
+    return resp_header + agent
 
 
 def main():
@@ -16,18 +41,18 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client, addr = server_socket.accept()
     msg = client.recv(1024)
-    headers = msg.split(b"\r\n")[0]
-    method, path, version = headers.split()
+    headers, *body = msg.split(b"\r\n\r\n")
+    headers_dict = parse_headers(headers)
 
     response = b""
-    echo_idx = path.find(b"/echo")
+    path = headers_dict['path']
 
     if path == b"/":
         response = OK + END
-    elif echo_idx != -1:
-        message = path[echo_idx + len("/echo/"):]
-        resp_header = OK + CONTENT_TYPE + gen_content_len(len(message)) + END
-        response = resp_header + message
+    elif (echo_idx := path.find(b"/echo")) != -1:
+        response = handle_echo(path, echo_idx)
+    elif path.find(b"/user-agent") != -1:
+        response = handle_user_agent(headers_dict['User-Agent'])
     else:
         response = NOT_FOUND + END
 
